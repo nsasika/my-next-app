3489c6686e707ba4ba742eb740ed0309d012c736
Nalin Padmasiri <nalinsasika9@gmail.com>
2026-06-08 15:54:49 +0530

    chore(next): migrate middleware to proxy convention

--- src/proxy.ts
+++ src/proxy.ts
@@ -12,7 +12,7 @@ const publicRoutes = [
   '/profilepic.png',
 ];
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const isPublicRoute = publicRoutes.some((route) => {
